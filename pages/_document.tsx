b8a5ff3f28e31ad2d775a83bdae57b98e39ca6df
eduuh <eduuh.muraya@outlook.com>
2024-08-31 13:57:39 +0300

    Add fire favicon: Will do for now

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,19 +10,19 @@ export default function Document() {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/images/favicon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/images/favicon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/images/avicon.png"
         />
         <link rel="manifest" href="/manifest.json" />
       </Head>
